a6f403db086afa5b5d2554ba05ce665f9e87fcd8
Naftali Ferreira <108247621+naftaliferreira@users.noreply.github.com>
2026-01-22 01:55:02 -0300

    Setup Flask application factory and core extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from .config import Config
-from .extensions import db
+from .extensions import db, migrate, jwt
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Inicializa extensões
     db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
 
-    from .routes.task_routes import task_bp
-    app.register_blueprint(task_bp)
+    # Blueprints
+    from .routes.health_routes import health_bp
+    app.register_blueprint(health_bp)
 
     return app
